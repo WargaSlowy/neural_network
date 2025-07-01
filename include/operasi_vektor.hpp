b675150382e31082385381680a21c5c777dc76ed
slowy07 <slowy.arfy@proton.me>
2025-07-01 21:23:49 +0700

    chore: menambahkan fungsi vektor
    - pop front -> menghapus elemen pertama pada vektor
    - pop back -> mengahpus elemen terakhir pada vektor
    - equal shuffle -> mengacak data elemen agar bersamaan
    
    link pembelajaran: https://youtube.com/live/zMdrMjw1DqM?feature=share
    
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

--- include/operasi_vektor.hpp
+++ include/operasi_vektor.hpp
@@ -1,9 +1,13 @@
 #ifndef OPERASI_VEKTOR_HPP_
 #define OPERASI_VEKTOR_HPP_
 
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
 #include <ostream>
+#include <utility>
 #include <valarray>
+#include <vector>
 
 /**
  * @brief menampilkan kostum output ke output stream std::cout
@@ -76,6 +80,83 @@ std::valarray<T> insert_element(const std::valarray<T> &A, const T &elemen) {
   // kita tambahin nilai baru di elemen si B
   B[B.size() - 1] = elemen;
   return B;
+}
+
+/**
+ * @brief fungsi untuk menghapus element pertama yang terdapat pada array
+ *
+ * @tparam T template parameter yang akan menampung nilai numerik
+ * @param A vektor atau array yang akan diberikan nantinya
+ * @return std::valarray<T> hasil penghapusan dari elemen pertama
+ */
+template <typename T> std::valarray<T> pop_front(const std::valarray<T> &A) {
+  // membuat valarray baru
+  std::valarray<T> B;
+  // resize dengan ukuran dikurangkan dengan 1
+  B.resize(A.size() - 1);
+  // looping untuk mengkopi data dari A ke B
+  for (size_t i = 1; i < A.size(); ++i) {
+    B[i - 1] = A[i];
+  }
+
+  return B;
+}
+
+/**
+ * @brief fungsi untuk menghapus elemen terakhir yang terdapat pada array atau
+ * list
+ *
+ * @tparam T parameter yang menampung jenis nilai numerik
+ * @param A vektor atau list yang diberikan
+ * @return std::valarray<T> hasil baru dengan menghapus elemen terakhir yang
+ * terdapat pada list
+ */
+template <typename T> std::valarray<T> pop_back(const std::valarray<T> &A) {
+  // membuat valarray baru
+  std::valarray<T> B;
+  // resize B dengan size dikurangkan dengan 1
+  B.resize(A.size() - 1);
+  // copy elemen A ke B dengan menghapus elemen terakhir
+  for (size_t i = 0; i < A.size() - 1; i++) {
+    B[i] = A[i];
+  }
+  return B;
+}
+
+/**
+ * @brief fungsi equal shuffle
+ *
+ * fungsi ini bertujuan juga untuk nantinya model tidak terbias dengan urutan
+ * pada array tertentu, jadi kita akan mengacak data array beserta labelnya
+ * secara bersamaan
+ *
+ * @tparam T template paramater yang akan dimasukkan tipe data nilai
+ * @param A input data (misalnya fitur atau citra)
+ * @param B input data tapi untuk label (hasil prediksi)
+ */
+template <typename T>
+void equal_shuffle(std::vector<std::vector<std::valarray<T>>> &A,
+                   std::vector<std::vector<std::valarray<T>>> &B) {
+  // cek apakah size dari A dan B itu sama
+  if (A.size() != B.size()) {
+    std::cerr << "ERROR di fungsi " << __func__ << ": ";
+    std::cerr << "tidak bisa shuffeling dua vektor dengan size yang berbeda ";
+    std::cerr << A.size() << " dan " << B.size() << std::endl;
+    std::exit(EXIT_FAILURE);
+  }
+
+  for (size_t i = 0; i < A.size(); i++) {
+    // untuk menetapkan seed random berdasarkan waktu sistem
+    std::srand(std::chrono::system_clock::now().time_since_epoch().count());
+    // menghasilkan angka acak antara 0 sampai size dari A - 1
+    size_t random_index = std::rand() % A.size();
+    // swap elemen ke i dengan elemen acak di vektor A dan B secara sama
+    // fungsi swap ini memastikan bahwa pair data tetap cocok setelah
+    // di random
+    std::swap(A[i], A[random_index]);
+    std::swap(B[i], B[random_index]);
+  }
+  return;
 }
 
 #endif // !OPERASI_VEKTOR_HPP_
--- test.cpp
+++ test.cpp
@@ -1,17 +1,54 @@
 #include "include/operasi_vektor.hpp"
+#include <iterator>
 #include <utility>
 #include <valarray>
+#include <vector>
 
 int main() {
 
-  auto p = std::make_pair(2.6712727417247124, 2.781238192839182);
-  std::cout << p << std::endl;
-  std::valarray<double> array_kita = {2.34689, 4.2678, 3.3333333, 2.3, 4.2, 5.5, 6.7};
-  std::cout << array_kita;
+  // auto p = std::make_pair(2.6712727417247124, 2.781238192839182);
+  // std::cout << p << std::endl;
+  // std::valarray<double> array_kita =
+  // {2.34689, 4.2678, 3.3333333, 2.3, 4.2, 5.5, 6.7}; std::cout << array_kita;
+  //
+  // std::valarray<int> array_pertama = {1, 2, 3};
+  // auto array_kedua = insert_element(array_pertama, 4);
+  // std::cout << array_kedua;
 
-  std::valarray<int> array_pertama = {1, 2, 3};
-  auto array_kedua = insert_element(array_pertama, 4);
-  std::cout << array_kedua;
+  // std::valarray<int> data_kita = {20, 30, 40, 50};
+  // std::cout << "sebelum fungsi pop front: " << data_kita << std::endl;
+  // data_kita = pop_front(data_kita);
+  // std::cout << "sesudah fungsi dari pop front: " << data_kita << std::endl;
+  // std::cout << "sebelum fungsi dari pop back: " << data_kita << std::endl;
+  // data_kita = pop_back(data_kita);
+  // std::cout << "sesudah fungsi dari pop back: " << data_kita << std::endl;
+
+  std::vector<std::vector<std::valarray<double>>> A = {
+      {{1.1, 2.2}}, {{3.3, 4.4}}, {{5.5, 6.6}}};
+
+  std::vector<std::vector<std::valarray<double>>> B = {
+      {{10.0}},
+      {{20.0}},
+      {{30.0}},
+  };
+
+  std::cout << "sebelum adanya shuffling:\n";
+  for (size_t i = 0; i < A.size(); ++i) {
+    std::cout << "A[" << i << "] = {" << A[i][0][0] << ", " << A[i][0][1]
+              << "} -> ";
+    std::cout << "B[ " << i << "] = {" << B[i][0][0] << "}\n";
+  }
+
+  equal_shuffle(A, B);
+  std::cout << std::endl;
+  std::cout << "setelah shuffle:";
+  for (size_t i = 0; i < A.size(); ++i) {
+    std::cout << "A[" << i << "] = {" << A[i][0][0] << ", " << A[i][0][1]
+              << "} -> ";
+    std::cout << "B[ " << i << "] = {" << B[i][0][0] << "}\n";
+  }
+
+  hitung::angka;
 
   return 0;
 }
