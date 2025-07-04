ba300a46382b15c25317f427774c6ad195d7fde6
slowy07 <slowy.arfy@proton.me>
2025-07-04 22:45:23 +0700

    chore: menambahkan fungsi aktivasi
    informasi stream: https://youtube.com/live/8XEBzs3ghGc?feature=share
    
    Signed-off-by: slowy07 <slowy.arfy@proton.me>

---
+++ include/function.hpp
@@ -0,0 +1,65 @@
+#ifndef FUNCTION_HPP_
+#define FUNCTION_HPP_
+
+#include <cmath>
+namespace FungsiAktivasi {
+
+/**
+ * @brief fungsi sigmoid
+ *
+ * fungsi ini mengambil nilai numerik x dan nantinya akan dirubah
+ * menjadi antara nilai 0 dan 1
+ *
+ * @param x nilai logit yang diberikan
+ * @return double hasil kalkulasi sigmoid
+ */
+double sigmoid(const double &x) { return 1.0 / (1.0 + std::exp(-x)); }
+
+/**
+ * @brief fungsi turunan dari sigmoid
+ *
+ * fungsi ini nantinya akan digunakan untuk menghitung gradient error terhadapat
+ * si bobot (backpropagation) dan juga nantinya akan mengupdate parameter si
+ * model selama fase training
+ *
+ * @param y input yang merupakan hasil dari output si sigmoid
+ * @return double hasil dari turunan sigmoid
+ */
+double sigmoid_derivatif(const double &y) { return y * (1.0 - y); }
+
+/**
+ * @brief fungsi relu untuk mencegah / mengurangi terjadinya vanishing gradient
+ *
+ * @param x nilai yang diberikan
+ * @return double hasil jika lebih dari input maka outputnya input itu sendiri
+ * jika kurang dari 0, maka 0
+ */
+double relu(const double &x) { return std::max(0.0, x); }
+
+/**
+ * @brief fungsi relu derivatif
+ *
+ * @param x nilai yang diberikan output dari fungsi relu
+ * @return double hasil dari turunan fungsi relu derivatif
+ */
+double relu_derivatif(const double &x) { return x >= 0.0 ? 1.0 : 0.0; }
+
+/**
+ * @brief fungsi aktivasi tanh untuk menghasilkan nilai rentang antara -1 sampai
+ * 1
+ *
+ * @param x nilai yang akan dihitung
+ * @return double akan menghasilkan 1 jika positif besar 0 jika positif kecil
+ */
+double tanh(const double &x) { return 2.0 / (1.0 + std::exp(-2.0 * x)) - 1.0; }
+
+/**
+ * @brief fungsi turunan dari tanh
+ *
+ * @param y nilai output dari fungsi tanh yang dihitung
+ * @return double hasil dari turunan fungsi tanh
+ */
+double tanh_derivatif(const double &y) { return 1.0 - y * y; }
+
+} // namespace FungsiAktivasi
+#endif // !FUNCTION_HPP_
---
+++ test_kedua.cpp
@@ -0,0 +1,26 @@
+#include "include/function.hpp"
+#include <iostream>
+
+int main() {
+
+  // double x = 0;
+  // double y = FungsiAktivasi::sigmoid(x);
+  // double hasil = FungsiAktivasi::sigmoid_derivatif(y);
+  // std::cout << "hasilnya adalah " << hasil << std::endl;
+
+  // double x = 4.5;
+  // double hasil = FungsiAktivasi::relu(x);
+  // std::cout << "hasil dari relu adalah: " << hasil << std::endl;
+
+  // double relu = FungsiAktivasi::relu(-2);
+  // double turunan_relu = FungsiAktivasi::relu_derivatif(relu);
+  //
+  // std::cout << "hasilnya adalah: " << turunan_relu << std::endl;
+
+  // double x = 2.0;
+  // double hasil = FungsiAktivasi::tanh(x);
+  // double hasil_turunan = FungsiAktivasi::tanh_derivatif(hasil);
+  // std::cout << "hasilnya adalah: " << hasil_turunan << std::endl;
+
+  return 0;
+}
